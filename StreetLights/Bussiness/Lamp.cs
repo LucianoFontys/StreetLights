1d741188b40ebfcffd24a3d0e1a0086cbdcf9255
Llera,Luciano L.M.A. <l.llera@student.fontys.nl>
2025-03-16 22:21:29 +0100

    Added some Classes

---
+++ StreetLights/Bussiness/Lamp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetLights.Bussiness
+{
+    class Lamp
+    {
+        private int id;
+        private bool status;
+        private string location;
+        private List<Schedule> schedules;
+
+    }
+}
---
+++ StreetLights/Bussiness/Schedule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetLights.Bussiness
+{
+    class Schedule
+    {
+        private int id;
+        private int lightStrength;
+        private int LightStrengtMovement;
+        private DateTime start;
+        private DateTime end;
+    }
+}
